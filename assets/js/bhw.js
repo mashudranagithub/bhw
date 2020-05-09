eb7a849a029b4435dffaa984507ea42a47521ec1
mashudranagithub <mashudranabitm@github.com>
2020-05-09 15:46:57 +0600

    Created Repository

---
+++ assets/js/bhw.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+
+
+	$('.carousel').carousel({
+	  interval: 2000
+	})
+
+
+
+
+
+	    // Code for Main Slider
+    $('.work-slider').owlCarousel({
+        // items:3,
+        merge:true,
+        loop:true,
+        autoplay:true,
+        autoplaySpeed:2000,
+        autoplayTimeout:10000,
+        smartSpeed:500,
+        margin:30,
+        nav:false,
+        navDots:true,
+        lazyLoad:true,
+        center:true,
+        responsive:{
+            480:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+
+
+
+    //Code for sticky menu
+
+$(document).ready(function(){
+    var x = $('.header-main').offset().top; 
+    
+    $(window).scroll(function(){
+        var y = $(window).scrollTop();
+        
+        if(y>x){
+            $('.header-main').addClass('sticky');
+           }else{
+            $('.header-main').removeClass('sticky');
+           }
+    });
+    
+    $('.header-main').wrapAll('<div class="cover">');
+    $('.cover').css('height', $('.header-main').outerHeight());
+    
+});
+
+
+//jQuery Code for back to top
+$(window).scroll(function(){    
+    var wScroll = $(this).scrollTop();
+
+    var showScrollButton = 5;
+
+    if(wScroll > showScrollButton){
+        $('#top').fadeIn();  
+    }else{
+        $('#top').fadeOut();
+    }
+
+});
+
+
+//creating clidk function
+$('#top').click(function(){
+    $('body, html').animate({
+       scrollTop:0 
+    }, 2000);
+    return false;
+});
+
+
+
+
+
+
+
+
+	
+});
